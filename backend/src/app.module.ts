3e9411accc40693ccb997ffe2a72a5b82c75b048
Jorge <103595844+jorgelucasp42@users.noreply.github.com>
2025-01-20 20:00:20 -0300

    update

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PessoaModule } from './modules/pessoa/pessoa.module';
+import { ClienteModule } from './modules/cliente/cliente.module';
+import { PrestadorModule } from './modules/prestador/prestador.module';
+import { PetModule } from './modules/pet/pet.module';
+import { ServicoModule } from './modules/servico/servico.module';
+import { SolicitacaoModule } from './modules/solicitacao/solicitacao.module';
+import { EnderecoModule } from './modules/endereco/endereco.module';
+import { GaleriaModule } from './modules/galeria/galeria.module';
+import { AvaliacaoModule } from './modules/avaliacao/avaliacao.module';
+import { DisponibilidadeModule } from './modules/disponibilidade/disponibilidade.module';
 
 @Module({
-  imports: [],
+  imports: [PessoaModule, ClienteModule, PrestadorModule, PetModule, ServicoModule, SolicitacaoModule, EnderecoModule, GaleriaModule, AvaliacaoModule, DisponibilidadeModule],
   controllers: [AppController],
   providers: [AppService],
 })
