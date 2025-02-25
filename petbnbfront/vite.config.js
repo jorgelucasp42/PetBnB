1451281ceda4ab29f39eb61df246ba4d2e9633d9
RafaelSimasS <rafaeldesousasimas@gmail.com>
2025-02-24 22:41:02 -0300

    feat: adicionar configuração do Vite e atualizar scripts no package.json

---
+++ petbnbfront/vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+export default defineConfig({
+    plugins: [vue()],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'),
+        },
+        extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json'],
+    }
+})
